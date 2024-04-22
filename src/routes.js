4d40bc6ad24aa444683279712a6821496b68b211
metrobruno <brunotolentino67@gmail.com>
2024-04-21 22:26:39 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    // Envie uma resposta simples para o caminho raiz
+    res.send('Olá do seu servidor Node.js!');
+  });
+
 module.exports = router
