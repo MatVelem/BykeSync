16573a52fa2da3655ed7380761cbd68ae4ace0e2
metrobruno <brunotolentino67@gmail.com>
2024-04-24 22:58:52 -0300

    troca

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'compra_bicicletas' // Substitua por 'seu_banco_de_dados' se necessário
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/auth');
+const usersController = require('../controllers/users');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res) => {
-    // Envie uma resposta simples para o caminho raiz
-    res.send('Olá do seu servidor Node.js!');
-  });
+// Rotas de autenticação
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
-module.exports = router
+// Rotas de usuários (exemplos)
+router.get('/users', usersController.listUsers);
+router.post('/users', usersController.createUser);
+router.get('/users/:id', usersController.getUser);
+router.put('/users/:id', usersController.updateUser);
+router.delete('/users/:id', usersController.deleteUser);
+
+// Rotas de produtos (exemplos)
+router.get('/products', productsController.listProducts);
+router.post('/products', productsController.createProduct);
+router.get('/products/:id', productsController.getProduct);
+router.put('/products/:id', productsController.updateProduct);
+router.delete('/products/:id', productsController.deleteProduct);
+
+// ... (Rotas para outras funcionalidades do seu aplicativo)
+
+module.exports = router;
