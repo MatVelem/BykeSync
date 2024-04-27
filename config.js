e6b380f56dd01bae32490c2326896556c0e77caa
MatVelem <95192372+MatVelem@users.noreply.github.com>
2024-04-27 16:43:16 -0300

    atualização

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'compra_bicicleta' 
+};
---
+++ database.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql2'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function getNotes() {
+  const [rows] = await pool.query("SELECT * FROM bicicletas")
+  return rows
+}
+
+export async function getNote(id) {
+  const [rows] = await pool.query(`
+  SELECT * 
+  FROM bicicletas
+  WHERE id = ?
+  `, [id])
+  return rows[0]
+}
+
+export async function createNote(marca, modelo, cor, preco, estoque, foto, descricao) {
+    const [result] = await pool.query(`
+      INSERT INTO bicicletas (marca, modelo, cor, preco, estoque, foto, descricao)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `, [marca, modelo, cor, preco, estoque, foto, descricao]);
+    const id = result.insertId;
+    return getNote(id);
+  }
+  
