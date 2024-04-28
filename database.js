fc153f5bb9a82091ff606e3e3fe12b16214e338d
metrobruno <brunotolentino67@gmail.com>
2024-04-27 22:20:05 -0300

    implementação factory

--- database.js
+++ database.js
@@ -1,34 +1,36 @@
 import mysql from 'mysql2';
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-host: process.env.MYSQL_HOST,
-user: process.env.MYSQL_USER,
-password: process.env.MYSQL_PASSWORD,
-database: process.env.MYSQL_DATABASE
-}).promise()
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise();
 
 async function mostrarbicicleta(callback) {
-
-const [rows] = await pool.query('Select * FROM bicicletas')
-return [rows]
+  const [rows] = await pool.query('SELECT * FROM bicicletas');
+  return rows;
 }
 
 async function pegarbicicleta(id) { 
-    const [rows] = await pool.query(' SELECT * FROM bicicletas WHERE id = ? ', [id] )
-    return rows[0]
+  const [rows] = await pool.query('SELECT * FROM bicicletas WHERE id = ?', [id]);
+  return rows[0];
 }
 
-
-async function criarbicicleta(id, marca, modelo, cor, preco, estoque, foto, descricao) {
-  const result =  await pool.query('INSERT INTO bicicletas (marca, modelo, cor, preco, estoque, foto, descricao) VALUES (?, ?, ?, ?, ?, ?, ?) ', [marca, modelo, cor, preco, estoque, foto, descricao])
-    return result.insertId
-
+async function criarbicicleta(marca, modelo, cor, preco, estoque, foto, descricao) {
+  const result = await pool.query('INSERT INTO bicicletas (marca, modelo, cor, preco, estoque, foto, descricao) VALUES (?, ?, ?, ?, ?, ?, ?)', [marca, modelo, cor, preco, estoque, foto, descricao]);
+  return result.insertId;
 }
-const mostrar = await mostrarbicicleta();
 
-//const criar =  await criarbicicleta("4m","caloi","vermelho",4500.45,4,null,"melhor bike do mercado");
-//const pegar = await pegarbicicleta(3);
-console.log(mostrar)
-export { mostrarbicicleta }
+// Envolve o código que usa o await em uma função assíncrona
+async function iniciar() {
+  const mostrar = await mostrarbicicleta();
+  console.log(mostrar);
+}
+
+// Chama a função assíncrona para iniciar o processo
+iniciar();
+
+export { mostrarbicicleta, pegarbicicleta, criarbicicleta };
