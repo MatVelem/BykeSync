e5eac6f7f3e8e50881ffd934ffc1b2d8e1f61245
MatVelem <95192372+MatVelem@users.noreply.github.com>
2024-04-28 20:05:30 -0300

    Update app.js
    Melhorando a conexão de rotas/databse

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ import mysql from 'mysql2';
 import config from './config.js';
 import bodyParser from 'body-parser';
 
+
+const databaseHost = config.host;
+const databaseUser = config.user;
+const databasePassword = config.password;
+const databaseName = config.database;
+
+function database(config) {
+  const connection = mysql.createConnection(config);
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      throw new Error('Database connection failed');
+    }
+    console.log('Connected to the database successfully');
+  });
+  return connection;
+}
+
 const app = express();
+
+
+app.use(express.json());
+
+// Use as rotas definidas no arquivo routes.js
+
+
+export default app; // Exporte o app como padrão
 
 // Middleware para processar corpos de requisição
 app.use(bodyParser.json());
