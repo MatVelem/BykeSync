df4c9a289cf6f19eb610ed94291426617c38a9ba
metrobruno <brunotolentino67@gmail.com>
2024-05-03 23:27:12 -0300

    Update test.js

--- testes/test.js
+++ testes/test.js
@@ -11,15 +11,15 @@ describe('mostrarbicicleta', () => {
 });
 
 describe('criarbicicleta', () => {
-  let idCriado; // Define a variável aqui para que esteja disponível em todo o escopo da descrição do teste
+  let idCriado; 
 
   test('Deve criar uma bicicleta e mostrar o id', async () => {
     const result = await criarbicicleta('bruno','mateus','roxo',15458.4,5,null,'pelo amor de deus');
     expect(result).toBeDefined();
-    idCriado = result; // Salva o ID criado para posterior exclusão
+    idCriado = result; 
   });
 
-  // Após o teste, apaga o registro criado para limpar o ambiente de teste
+
   afterAll(async () => {
     if (idCriado) {
       await apagarBicicleta(idCriado);
